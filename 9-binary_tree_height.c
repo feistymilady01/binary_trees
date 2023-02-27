34b9be79d738e97ea0dc3cceebab687d7f1a6358
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-02-27 07:56:21 +0100

    tree height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,1 +1,39 @@
-ii
+#include "binary_trees.h"
+
+
+/**
+ * max - returns the max of two size_t numbers
+ *
+ * @left: number of the left
+ * @right: number on the right
+ *
+ * Return: the max number
+ */
+
+size_t max(size_t left, size_t right)
+{
+	if (left >= right)
+		return (left);
+
+	else
+		return (right);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ *
+ * @tree: pointer to the root node of the tree to measure the height
+ *
+ * Return: 0 if tree is NULL, otherwise returns the height of the tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	else if (!tree->left && !tree->right)
+		return (0);
+
+	return (max(binary_tree_height(tree->left),
+		binary_tree_height(tree->right)) + 1);
+}
