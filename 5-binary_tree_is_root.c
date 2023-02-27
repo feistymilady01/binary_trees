d3bc997073442ff1607ebe19b08a72628781ff01
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-02-27 07:53:04 +0100

    root tree

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,1 +1,22 @@
-ii
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - checks if a give node is a root
+ *
+ * @node: pointer to the node to check
+ *
+ * Return: 1 if node is a root,
+ *	0 if not and if node is NULL
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+
+	if (!node->parent)
+		return (1);
+
+	else
+		return (0);
+}
