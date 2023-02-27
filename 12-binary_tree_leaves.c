e84009edeabb3103e668c006fe0469724253a3fd
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-02-27 07:43:43 +0100

    tree leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,2 +1,31 @@
-ii
-ii
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to count the number of leaves
+ *
+ * Return: Number of leaves
+ * Otherwise: if tree is NULL return 0
+ *
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t left = 0, right = 0;
+
+	if (!tree)
+	{
+		return (0);
+	}
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+
+	left = binary_tree_leaves(tree->left);
+	right = binary_tree_leaves(tree->right);
+
+	return (left + right);
+}
