3aad63a156114923ed56853f19a478f3c08ce5cd
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-03-02 10:49:15 +0100

    tree balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,70 +1,38 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - Measure the balance factor of a binary tree
- * binary factor is the difference between the Height of the left
- * sub tree and the height of the right sub tree
+ * binary_tree_balance - Measures the balance factor of a binary tree.
  *
- * @tree: Pointer to the root node of the tree to measure the balsnce factor
+ * @tree: A pointer to the root node of the tree to measure the balance factor.
  *
- * Return: The Binary factor
- * Otherwise: return 0; if tree is NULL
- *
+ * Return: 0 if tree is NULL.
  */
-
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height = 0, right_height = 0;
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
-	if (!tree)
-	{
-		return (0);
-	}
-
-	if (tree->left)
-	{
-		left_height = 1 + binary_tree_height(tree->left);
-	}
-	if (tree->right)
-	{
-		right_height = 1 + binary_tree_height(tree->right);
-	}
-
-	return (left_height - right_height);
+	return (0);
 }
 
 /**
- * binary_tree_height - Measures the height of a binary tree
+ * binary_tree_height - Measures the height of a binary tree.
  *
- * @tree: Pointer to the root node of the tree to measure the height
+ * @tree: A pointer to the root node of the tree to measure the height.
  *
- * Return: Mamimum height
+ * Return: 0 if tree is NULL.
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0;
+	if (tree)
+	{
+		size_t lef = 0, rig = 0;
 
-	if (!tree)
-	{
-		return (0);
-	}
+		lef = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		rig = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 
-	if (tree->left)
-	{
-		left_height = 1 + binary_tree_height(tree->left);
-	}
-	if (tree->right)
-	{
-		right_height = 1 + binary_tree_height(tree->right);
+		return ((lef > rig) ? lef : rig);
 	}
 
-	if (left_height > right_height)
-	{
-		return (left_height);
-	}
-	else
-	{
-		return (right_height);
-	}
+	return (0);
 }
