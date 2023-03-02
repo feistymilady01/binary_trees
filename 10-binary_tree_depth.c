9a85ff6af5bb5c56f2136a3b6e7681e9480815de
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-03-02 09:41:05 +0100

    tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,29 +1,13 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - Measures the depth of a node in a binary tree
+ * binary_tree_depth - Measures the depth of a node in a binary tree.
  *
- * @tree: Pointer to the node to measure its depth
+ * @tree: A pointer to the node to measure the depth.
  *
- * Return: Depth of the selected node
- * Otherwise: if tree is NULL, return 0
- *
+ * Return: 0 if tree is NULL.
  */
-
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t depth = 0;
-
-	if (!tree)
-	{
-		return (0);
-	}
-
-	while (tree->parent)
-	{
-		depth++;
-		tree = tree->parent;
-	}
-
-	return (depth);
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
 }
